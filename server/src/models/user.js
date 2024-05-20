a3746021fad9c014de75d391290d33fc6500779b
Vasavi S <81638871+vasavi-s@users.noreply.github.com>
2024-05-21 02:37:32 +0530

    Create

---
+++ server/src/models/user.js
@@ -0,0 +1,24 @@
+// src/models/user.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true },
+  phoneNumber: String,
+  password: String,
+  isSeller: Boolean,
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
