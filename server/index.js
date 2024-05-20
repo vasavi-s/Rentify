64cb713ea2d093b3cb1cc02f779dc4216e707bcb
Vasavi S <81638871+vasavi-s@users.noreply.github.com>
2024-05-21 03:19:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,13 +11,7 @@ const app = express();
 const PORT = 3001;
 const MONGODB_URI = 'mongodb+srv://admin:secret123@cluster0.1nvhidr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-app.use(cors(
-  {
-    origin:[""],
-    methods:["POST", "GET"],
-    credentials: true
-  }
-));
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
 app.use('/properties', auth, propertyRoutes);
