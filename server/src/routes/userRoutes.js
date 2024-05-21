c13ef428f046730aefa2733ca27bfa1fbb2c5cf9
Vasavi S <81638871+vasavi-s@users.noreply.github.com>
2024-05-21 12:15:21 +0530

    Update userRoutes.js

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -4,9 +4,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get("/", (req,res)=>{
-  res.json("hello");
-})
+
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 
